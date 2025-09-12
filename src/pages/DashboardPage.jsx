66c422f1a64cfd3d420441f4d45b492cdd5bb178
Ana Luisa <analuisaaugustob@gmail.com>
2025-09-12 12:42:09 -0300

    feat(pages): add Navbar in Dashboard and update layout structure

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,9 +1,14 @@
+import Navbar from "../components/Navbar";
+
 const DashboardPage = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>O resumo financeiro aparecerá aqui.</p>
-    </div>
+    <>
+      <Navbar />
+      <div>
+        <h1>Dashboard</h1>
+        <p>O resumo financeiro aparecerá aqui.</p>
+      </div>
+    </>
   );
 };
 
