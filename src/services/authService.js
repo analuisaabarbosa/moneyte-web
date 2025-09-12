e0f11a134881ea8f5c807c4bcf1b45889c0d641d
Ana Luisa <analuisaaugustob@gmail.com>
2025-09-12 12:32:47 -0300

    refactor(services): update login and register to use api instance

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,18 +1,11 @@
 import api from "./api";
 
 export const login = async (email, password) => {
-    try {
-        const response = await api.post('/users/login', { email, password });
+    const response = await api.post('/users/login', { email, password });
+    return response.data;
+};
 
-        if (response.data.token) {
-            localStorage.setItem('userToken', response.data.token);
-            api.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
-        }
-
-        return response.data;
-    } catch (error) {
-        localStorage.removeItem('userToken');
-        delete api.defaults.headers.common['Authorization'];
-        throw error;
-    }
+export const register = async (name, email, password) => {
+    const response = await api.post('/users/register', { name, email, password });
+    return response.data;
 };
